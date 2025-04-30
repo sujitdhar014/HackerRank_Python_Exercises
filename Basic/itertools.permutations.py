5012af7a0935f6a42b34764ee14fe9c83b5ce077
Sujit <sujitdhar014@gmail.com>
2025-04-30 20:38:38 +0530

    solved basic

---
+++ Basic/itertools.permutations.py
@@ -0,0 +1,26 @@
+'''You are given a string .
+Your task is to print all possible permutations of size  of the string in lexicographic sorted order.'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import permutations
+
+s,k=input().split()
+k=int(k)
+
+perm=sorted(permutations(s,k))
+
+for p in perm:
+    print("".join(p))
+
+'''>>> from itertools import permutations
+>>> print permutations(['1','2','3'])
+<itertools.permutations object at 0x02A45210>
+>>> 
+>>> print list(permutations(['1','2','3']))
+[('1', '2', '3'), ('1', '3', '2'), ('2', '1', '3'), ('2', '3', '1'), ('3', '1', '2'), ('3', '2', '1')]
+>>> 
+>>> print list(permutations(['1','2','3'],2))
+[('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2')]
+>>>
+>>> print list(permutations('abc',3))
+[('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]'''
