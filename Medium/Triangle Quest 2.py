2bb04b045b98435f0c51fd98baf272420ea6ad9e
Sujit <sujitdhar014@gmail.com>
2025-05-01 23:46:31 +0530

    solved problem

---
+++ Medium/Triangle Quest 2.py
@@ -0,0 +1,17 @@
+# for i in range(1,int(input())+1): 
+#     for i in range(i+1):
+#         print(i,end="")
+#     print(i-1)
+#     print ((((10**i)-1)//(9))**2)
+
+# 1
+# 121
+# 12321
+# 1234321
+# 123454321
+
+"""for i in range(1,int(input())+1): 
+     print ((((10**i)-1)//(9))**2)"""
+
+for i in range(1,int(input())+1): 
+     print ((((10**i)-1)//(9))**2)
